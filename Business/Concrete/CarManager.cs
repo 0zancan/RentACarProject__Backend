6decca7aec5afe05bd8fddba692326f4eb3357ea
Ozan Can <48280981+0zancan@users.noreply.github.com>
2021-09-07 20:36:39 +0300

    added validation

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -23,13 +25,9 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult AddCar(Car car)
         {
-            if (car.Name.Length < 2)
-            {
-                return new ErrorResult(Messages.CarNameInvalid);
-            }
-
             _carDal.Add(car);
             return new SuccessResult(Messages.CarAdded);
         }
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.Name).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalıdır!");
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+            // A ile başlıyorsa true döner hata almaz dönmezse hata alır!
+        }
+    }
+}
